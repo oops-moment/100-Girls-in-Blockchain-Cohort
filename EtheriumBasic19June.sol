3d04169c02eae7a88a6ec7af26ea15db9e9acd24
Prisha <112310373+oops-moment@users.noreply.github.com>
2023-06-19 20:45:49 +0530

    Create EtheriumBasic19June.sol
    Tranfer, TranferFrom, approval, allowance concept

---
+++ EtheriumBasic19June.sol
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.18;
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+interface IMintable {
+    function mint(address account, uint256 amount) external returns (bool);
+
+}
+
+interface Iburnable{
+    function burn (address account,uint256 ) external returns (bool);
+}
+contract MyToken is IERC20,IMintable,Iburnable {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    address owner;
+
+    uint256 private _totalSupply;
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 initialSupply) {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        _totalSupply = initialSupply;
+        _balances[msg.sender] = _totalSupply;
+        owner=msg.sender;
+    }
+
+    function totalSupply() public view override returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        require(_balances[msg.sender] >= amount, "Insufficient balance");
+        _balances[msg.sender] -= amount;
+        _balances[recipient] += amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function allowance(address _owner, address spender) public view override returns (uint256) {
+        return _allowances[_owner][spender];
+    }
+
+    function approve(address spender, uint256 amount) public override returns (bool) {
+        _allowances[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+        require(_balances[sender] >= amount, "Insufficient balance");
+        require(_allowances[sender][msg.sender] >= amount, "Insufficient allowance");
+        _balances[sender] -= amount;
+        _balances[recipient] += amount;
+        _allowances[sender][msg.sender] -= amount;
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+
+    function mint(address account, uint amount) external override returns(bool)
+    {
+        require(msg.sender==owner,"HATTT");
+        _totalSupply+=amount;
+        _balances[account]+=amount;
+        return true;
+    }
+
+    function burn(address account, uint amount) external override returns(bool)
+    {
+        require(msg.sender==owner,"HATTT");
+        _totalSupply-=amount;
+        _balances[account]-=amount;
+        return true;
+    }
+
+}
